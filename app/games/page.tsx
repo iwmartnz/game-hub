3ed88ddc83453fab3fee7a0428678bd4ae50e822
Isra <iwmartnz@gmail.com>
2024-04-08 12:18:34 -0600

    Created the games list home page

---
+++ app/games/page.tsx
@@ -0,0 +1,27 @@
+import { Suspense } from 'react';
+
+import GameCard from '@/components/GameCard';
+import Grid from '@/components/Grid';
+import Heading from '@/components/Heading';
+import Skeleton from '@/components/Skeleton';
+
+import { getAllGames } from '@/services/games';
+import { Game } from '@/types';
+
+export default async function GamesPage() {
+    const games: Game[] = await getAllGames();
+    return (
+        <main className='w-full space-y-2'>
+            <Heading type='heading'>All Games</Heading>
+            <Suspense fallback={<Skeleton variant='grid' />}>
+                <Grid className='py-6'>
+                    {games.map((game) => (
+                        <li key={game.id}>
+                            <GameCard game={game} />
+                        </li>
+                    ))}
+                </Grid>
+            </Suspense>
+        </main>
+    );
+}
