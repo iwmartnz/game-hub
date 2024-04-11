da44a69dac28a4d8c700f1630e01feebd534ff98
Isra <iwmartnz@gmail.com>
2024-04-11 15:02:39 -0600

    Added the Header component to the UI layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Urbanist } from 'next/font/google';
 import './globals.css';
 
+import Header from '@/components/Header';
 import Sidebar from '@/components/Sidebar';
 import BackgroundImage from '@/components/BackgroundImage';
 import { GameImageProvider } from '@/context/GameImageContext';
@@ -25,6 +26,7 @@ export default function RootLayout({
             >
                 <GameImageProvider>
                     <BackgroundImage />
+                    <Header />
                     <div className='flex gap-6 px-6'>
                         <Sidebar />
                         {children}
