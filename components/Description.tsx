9af8bb92741503e53a52b9ac82f7b3ac93f669e4
Isra <iwmartnz@gmail.com>
2024-04-15 09:28:01 -0600

    Refactor and added a button to trim the description size

--- components/Description.tsx
+++ components/Description.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { cn } from '@/utils/style';
 import parse from 'html-react-parser';
 import { useState } from 'react';
 
@@ -8,7 +9,37 @@ type GameDescriptionProps = {
 
 export default function Description({ rawDescription }: GameDescriptionProps) {
     const [showFullDescription, setShowFullDescription] = useState(false);
-    const fullDescription: any = parse(rawDescription);
+    const description: any = parse(rawDescription);
+    const isLargeDescription = rawDescription.length > 1200;
 
-    return <div className=' text-brand-gray-light'>{fullDescription}</div>;
+    function toggleDescription() {
+        setShowFullDescription((prevState) => !prevState);
+    }
+    return (
+        <div
+            className={cn(
+                'relative h-72 w-full overflow-hidden text-brand-gray-light',
+                showFullDescription && `h-auto w-full overflow-y-clip`
+            )}
+        >
+            {description}
+            {isLargeDescription && (
+                <div
+                    className={cn(
+                        'absolute bottom-0 flex h-20 w-full items-end justify-center bg-gradient-to-t from-brand-black to-black/0',
+                        showFullDescription && 'relative h-6 pt-8'
+                    )}
+                >
+                    <button
+                        className=' rounded-xl bg-brand-white px-2 py-1 text-sm text-brand-black'
+                        onClick={toggleDescription}
+                    >
+                        {showFullDescription === false
+                            ? `Read more`
+                            : `Show less`}
+                    </button>
+                </div>
+            )}
+        </div>
+    );
 }
